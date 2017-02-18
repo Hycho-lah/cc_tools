095020f63f37f1df3e60481ec1ea6df0ad2d09da
hycho_lah <elissa_ye@hotmail.com>
2017-02-17 21:25:17 -0500

    added command line

--- convert.py
+++ convert.py
@@ -1,4 +1,4 @@
 import cc_dat_utils
-cc_data = cc_dat_utils.make_cc_data_from_dat("data/pfgd_test.dat")
+cc_data = cc_dat_utils.make_cc_data_from_dat("data/eye_cc1.dat")
 print (str(cc_data))
 
--- eye_convert.py
+++ eye_convert.py
@@ -1,5 +1,23 @@
+import sys
+import json
+
 from cc_json_utils import make_cc_data_from_json #Import functions from other files
 from cc_dat_utils import write_cc_data_to_dat
 
-cc_dat = make_cc_data_from_json("data/eye_cc1.json") #input name of json file
-write_cc_data_to_dat(cc_dat, "eye_cc1.dat") #create dat file from json file
+default_input_json_file = "data/eye_cc1.json"
+default_output_dat_file = "data/eye_cc1.dat"
+
+if len(sys.argv) == 3:
+    input_json_file = sys.argv[1]
+    output_dat_file = sys.argv[2]
+    print("Using command line args:", input_json_file, output_dat_file)
+else:
+    print("Unknown command line options. Using default values:", default_input_json_file, default_output_dat_file)
+    input_json_file = default_input_json_file
+    output_dat_file = default_output_dat_file
+
+cc_dat = make_cc_data_from_json(input_json_file) #input name of json file
+write_cc_data_to_dat(cc_dat, output_dat_file)  # create dat file from json file
+
+
+
